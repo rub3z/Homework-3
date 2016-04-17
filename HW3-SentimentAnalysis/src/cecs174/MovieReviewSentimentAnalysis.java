cc65eb3f1dab45efa7ca6488d9a7a6aba6b0e13c
rub3z <rubeswift@gmail.com>
2016-04-17 16:49:47 -0700

    HOLY SHIT IT WORKS NOW OMG

--- HW3-SentimentAnalysis/src/cecs174/MovieReviewSentimentAnalysis.java
+++ HW3-SentimentAnalysis/src/cecs174/MovieReviewSentimentAnalysis.java
@@ -9,17 +9,18 @@ public class MovieReviewSentimentAnalysis {
    public static void main(String[] args) 
    {
       // TODO: complete me!
-      System.out.println("Incomplete assignment");
-      
+     
       Scanner input = new Scanner(System.in);
       
-      System.out.println("Simon Cowell up in this bitch nigga!");
+      System.out.println("Please enter your movie review. \n" +
+    		  			 "Try to use proper grammar, however it's obviously not required to be a movie critic. ");
       String userReview = input.nextLine();
     		  
-      String[] movieReviews = new String[10];
-      int[] movieReviewScores = new int[10];
+      String[] movieReviews = new String[9000];
+      int[] movieReviewScores = new int[9000];
       
       MovieReviewReader.readMovieReviews( "movie_reviews.txt" , movieReviews , movieReviewScores );
+      
 //    String[] formattedReview = new String[500];  
 //          
 //      for(int i = 0; i < formattedReview.length ; i++) {
@@ -29,14 +30,15 @@ public class MovieReviewSentimentAnalysis {
 //      }
 //		All of this is here to test out the formatReview method on the movieReviews.txt file      
       
-      calculateReviewScore( calculateWordScores( userReview , movieReviews , movieReviewScores ) , userReview );
+      userReviewScore( calculateWordScores( formatReview( userReview ).split("\\s+") , movieReviews , movieReviewScores ) ,
+    		  								formatReview( userReview ).split("\\s+") );
       
       input.close();    
    }
    
-   public static String[] formatReview( String userReview )
+   public static String formatReview( String userReview )
    {
-	   String[] words = userReview.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+	   String words = userReview.replaceAll("[^a-zA-Z ]", "").toLowerCase();
 	   
 //	   for(int j = 0; j < words.length ; j++) {
 // 		  System.out.print(words[j] + " "); 		  
@@ -46,23 +48,34 @@ public class MovieReviewSentimentAnalysis {
 	   return words;
    }
    
-   public static double[] calculateWordScores( String userReview , String[] movieReviews , int[] movieReviewScores) {
-	   double wordScores[] = {};
+   public static double[] calculateWordScores( String[] editedUserReview , String[] movieReviews , int[] movieReviewScores) {
 	   
+	   double[] wordScores = new double[ editedUserReview.length ];
 	   
+	   double reviewContainsWord = 0 ;
+	   double reviewSum = 0;
 	   
-	   
-	   
-	   
+	   for( int i = 0; i < editedUserReview.length ; i++) {
+		   for( int j = 0; j < movieReviews.length ; j++ ) {
+			   if ( formatReview( movieReviews[j] ).contains( editedUserReview[i] ) ) {
+				   reviewSum += movieReviewScores[j];
+				   reviewContainsWord++;
+				
+			   }
+		   }
+		   if ( reviewContainsWord > 0 )
+			   wordScores[i] = reviewSum / reviewContainsWord;
+		   else
+			   wordScores[i] = 0;
+	   }
 	   
 	   return wordScores;
    }
    
-   public static double calculateReviewScore( double[] wordScores , String userReview ) {
+   public static void userReviewScore( double[] wordScores , String[] editedUserReview ) {
+	   
 	   double reviewScore = 0;
-	   
-	   String[] wordsInUserReview = formatReview( userReview );
-	   
+	   	   
 	   double minScore = 4 , 		maxScore = 0;
 	   double lowestWordScore = 0 , highestWordScore = 0 ;
 	   String lowestWord = "",		highestWord = "";
@@ -71,34 +84,38 @@ public class MovieReviewSentimentAnalysis {
 		   if ( wordScores[i] < minScore ) {
 			   minScore = wordScores[i];
 			   lowestWordScore = wordScores[i];
-			   lowestWord = wordsInUserReview[i];
+			   lowestWord = editedUserReview[i];
 		   }
 		   
 		   if ( wordScores[i] > maxScore ) {
 			   maxScore = wordScores[i];
 			   highestWordScore = wordScores[i];
-			   highestWord = wordsInUserReview[i];
+			   highestWord = editedUserReview[i];
 		   }
+		   System.out.println( wordScores[i] );
 		   
 		   reviewScore += wordScores[i];
 	   }
 	   
+	   System.out.println( reviewScore ); 
 	   
 	   System.out.println("The score of your review is " + (reviewScore / wordScores.length) );
 	   
 	   if ((reviewScore / wordScores.length) < 1.99 )
-			System.out.println("The overall sentiment of your review is negative." );
+			System.out.println("The overall sentiment of your review is negative. \n" +
+							   "You probably didn't like it because it's too smart for you.");
 	   else if ((reviewScore / wordScores.length) < 2.009 )
-			System.out.println("The overall sentiment of your review is neutral." );
+			System.out.println("The overall sentiment of your review is neutral.  \n" +
+							   "Don't you know it's boring to have a non-committal perspective?");
 	   else
-			System.out.println("The overall sentiment of your review is positive." );
+			System.out.println("The overall sentiment of your review is positive. \n" +
+								"It was pretty awesome, wasn't it?");
 
-	   System.out.println("The most positive word in your review is " + highestWord +
-			   			  " with a score of " + highestWordScore + "." );
-	   System.out.println("The most negative word in your review is " + lowestWord + 
-			   			  " with a score of " + lowestWordScore + "." );
-	   	   
-	   return reviewScore; // Yay this is all done
+	   System.out.println("The most positive word in your review is '" + highestWord +
+			   			  "' with a score of " + highestWordScore + "." );
+	   System.out.println("The most negative word in your review is '" + lowestWord + 
+			   			  "' with a score of " + lowestWordScore + "." );
+	   // Yay this is all done
    }
 }
 
